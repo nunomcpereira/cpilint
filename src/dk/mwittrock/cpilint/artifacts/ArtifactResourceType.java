064e6b8dccf0353ccbf471710dec8cdcf61fdfef
Flávio <flfu@ferring.com>
2024-01-10 16:01:59 +0100

    updated to 1.0.4

---
+++ src/dk/mwittrock/cpilint/artifacts/ArtifactResourceType.java
@@ -0,0 +1,27 @@
+package dk.mwittrock.cpilint.artifacts;
+
+public enum ArtifactResourceType {
+	
+	MESSAGE_MAPPING("message mapping"),
+	XSLT_MAPPING("XSLT mapping"),
+	OPERATION_MAPPING("operation mapping"),
+	JAVASCRIPT_SCRIPT("JavaScript script"),
+	GROOVY_SCRIPT("Groovy script"),
+	EXTERNAL_PARAMETERS("External Parameters"),
+	JAVA_ARCHIVE("Java archive"),
+	IFLOW("integration flow"),
+	EDMX("EDMX"),
+	WSDL("WSDL"),
+	XSD("XML Schema");
+
+	private final String name;
+	
+	private ArtifactResourceType(String name) {
+		this.name = name;
+	}
+	
+	public String getName() {
+		return name;
+	}
+	
+}
